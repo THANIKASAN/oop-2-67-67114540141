f53f40e17d2ca3cf9bd74d9518e2f0fb34d65e2a
THANIKASAN <tangmozaa10@gmail.com>
2025-01-24 14:04:06 +0700

    สร้างคลาสแรกบน Github repo 555

---
+++ app.py
@@ -0,0 +1,12 @@
+class student(object):
+  student_count = 0
+  def__new__(self):
+    print('student.__new__')
+  
+  def__init__(self):
+   print('student.__init__')
+  self.gpa = 4.0
+  
+  def study_hard(self):
+    print('sir,yes sir.')
+  
